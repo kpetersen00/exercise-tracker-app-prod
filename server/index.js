a48f953671f7b0357aa273422f35c6ab7270be60
Kyle Petersen <kyle-petersen@Kyles-MacBook-Air.local>
2022-03-30 10:01:36 -0700

    firebase config

--- server/index.js
+++ server/index.js
@@ -4,28 +4,30 @@ const morgan = require('morgan');
 const path = require('path');
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
 const routes = require('../routes/api');
 
 // Have Node serve the files for built React app
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
-console.log(process.env.MONGO_URI);
+
 mongoose.connection.on('connected', () => {
   console.log('mongoose is connected');
 });
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
+// // All other GET requests not handled before will return our React app
+// app.get('*', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+// });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
