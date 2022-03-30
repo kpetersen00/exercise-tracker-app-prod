ffcabb2a26b91ace40df6bbf7004a589f5ee9614
Kyle Petersen <kyle-petersen@Kyles-MacBook-Air.local>
2022-03-29 21:55:01 -0700

    edit firebase

--- server/index.js
+++ server/index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
-const path = require('path');
-
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3001;
@@ -11,6 +9,9 @@ const app = express();
 
 const routes = require('../routes/api');
 
+// Have Node serve the files for built React app
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -18,6 +19,11 @@ mongoose.connect(process.env.MONGO_URI, {
 console.log(process.env.MONGO_URI);
 mongoose.connection.on('connected', () => {
   console.log('mongoose is connected');
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
 app.use(express.json());
